d93ff2a3a205e3e8502050fceeabfc222f97c5ce
Taro Altricher <ta029@hdm-stuttgart.de>
2024-03-20 20:20:27 +0100

    created Login and Registration View

--- LF/LF/LFLoginView.swift
+++ LF/LF/LFLoginView.swift
@@ -8,8 +8,80 @@
 import SwiftUI
 
 struct LFLoginView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 220)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .textInputAutocapitalization(.never)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                NavigationLink {
+                    Text("Forgot password")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing, 28)
+                        .foregroundColor(.teal)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                }
+                
+                Button {
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.teal)
+                        .cornerRadius(8)
+                }
+                
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    
+                } label: {
+                    HStack {
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up Now!")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundStyle(.teal)
+                    .font(.footnote)
+                }
+                .padding(.vertical, 16)
+            }
+        }
     }
 }
 
--- LF/LF/LFRegistrationView.swift
+++ LF/LF/LFRegistrationView.swift
@@ -8,8 +8,84 @@
 import SwiftUI
 
 struct LFRegistrationView: View {
+    @State var email = ""
+    @State var password = ""
+    @State var username = ""
+    @State var fullname = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 220, height: 220)
+                .padding()
+            
+            VStack {
+                TextField("Enter your email", text: $email)
+                    .textInputAutocapitalization(.never)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                
+                TextField("Enter your username", text: $username)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                
+                TextField("Enter your full name", text: $fullname)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                
+                SecureField("Enter your password", text: $password)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+
+            }
+            
+            Button {
+                
+            } label: {
+                Text("Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.white)
+                    .frame(width: 352, height: 44)
+                    .background(.teal)
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            
+            Spacer()
+            
+            Divider()
+            
+            Button {
+                
+            } label: {
+                HStack {
+                    Text("Already have an account?")
+                    
+                    Text("Log In!")
+                        .fontWeight(.semibold)
+                }
+                .foregroundStyle(.teal)
+                .font(.footnote)
+            }
+            .padding(.vertical, 16)
+        }
     }
 }
 
