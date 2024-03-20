b2ccf3e536509fc98aa0c7bc041893ce0641ba69
Taro Altricher <ta029@hdm-stuttgart.de>
2024-03-20 17:17:38 +0100

    setup xcode project

---
+++ LF/LF/LFApp.swift
@@ -0,0 +1,17 @@
+//
+//  LFApp.swift
+//  LF
+//
+//  Created by Taro Altrichter on 20.03.24.
+//
+
+import SwiftUI
+
+@main
+struct LFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
