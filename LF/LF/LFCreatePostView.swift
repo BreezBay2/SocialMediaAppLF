6293622b3b94d29c60d79023457bc53f593581c4
Taro Altricher <ta029@hdm-stuttgart.de>
2024-03-21 01:45:13 +0100

    created UI and basic functionality for CreatePostView

--- LF/LF/LFCreatePostView.swift
+++ LF/LF/LFCreatePostView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct LFCreatePostView: View {
+    @State private var caption = ""
+    
     var body: some View {
-        Text("Create Post")
+        NavigationStack {
+            VStack {
+                HStack {
+                    Image(systemName: "person.circle.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 64, height: 64)
+                        .foregroundStyle(Color(.systemGray4))
+                    
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text("@cutekofi")
+                            .fontWeight(.semibold)
+                        
+                        TextField("What's Up?", text: $caption, axis: .vertical)
+                    }
+                    
+                    Spacer()
+                    
+                    if !caption.isEmpty {
+                        Button {
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                            
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Post")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.teal)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1.0)
+                    .disabled(caption.isEmpty)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.teal)
+                }
+            }
+        }
     }
 }
 
--- LF/LF/LFTabView.swift
+++ LF/LF/LFTabView.swift
@@ -8,33 +8,54 @@
 import SwiftUI
 
 struct LFTabView: View {
+    @State private var selectedTab = 0
+    @State private var showCreatePostView = false
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             LFFeedView()
                 .tabItem {
                     Image(systemName: "house.fill")
                 }
+                .onAppear { selectedTab = 0 }
+                .tag(0)
             
             LFSearchView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                 }
+                .onAppear { selectedTab = 1 }
+                .tag(1)
             
-            LFCreatePostView()
+            Text("")
                 .tabItem {
                     Image(systemName: "plus")
                 }
+                .onAppear { selectedTab = 2 }
+                .tag(2)
             
             LFActivityView()
                 .tabItem {
                     Image(systemName: "heart.fill")
                 }
+                .onAppear { selectedTab = 3 }
+                .tag(3)
             
             LFCurrentUserProfileView()
                 .tabItem {
                     Image(systemName: "person.fill")
                 }
+                .onAppear { selectedTab = 4 }
+                .tag(4)
         }
+        .onChange(of: selectedTab, perform: { newValue in
+            showCreatePostView = selectedTab == 2
+        })
+        .sheet(isPresented: $showCreatePostView, onDismiss: {
+            selectedTab = 0
+        }, content: {
+            LFCreatePostView()
+        })
         .tint(.teal)
     }
 }
